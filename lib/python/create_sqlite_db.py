1d71d182b9db1e7ede8c8fd50f42102b4f90ac66
Yunze Wang <39050699+Firefox2100@users.noreply.github.com>
2023-02-06 16:55:55 +0000

    SQLite generation script added

---
+++ lib/python/create_sqlite_db.py
@@ -0,0 +1,86 @@
+import sqlite3
+import requests
+import json
+
+db = sqlite3.connect('amiibo.db')
+print ("Successfully opened database")
+c = db.cursor()
+
+
+def create_tables():
+    c.execute('''CREATE TABLE amiibos
+       (id INT NOT NULL,
+       name TEXT NOT NULL,
+       head TEXT NOT NULL,
+       tail TEXT NOT NULL,
+       characters TEXT NOT NULL,
+       type TEXT NOT NULL,
+       PRIMARY KEY(id)
+       );''')
+
+    c.execute('''CREATE TABLE usages
+       (id INT NOT NULL,
+       amiibo TEXT NOT NULL,
+       game TEXT NOT NULL,
+       platform TEXT NOT NULL,
+       usage TEXT NOT NULL,
+       write INT NOT NULL,
+       PRIMARY KEY(id)
+       );''')
+
+    print ("Successfully created tables")
+
+    db.commit()
+
+
+def construct_amiibos():
+    amiibo_data = []
+    usage_data = []
+    
+    with open("amiibo.json", "r") as json_in:
+        api_data = requests.get("https://amiiboapi.com/api/amiibofull/")
+        json_data = api_data.json()["amiibo"]
+        i = 0
+        j = 0
+
+        for item in json_data:
+            amiibo_data.append((i, item["name"], item["head"], item["tail"], item["character"], item["type"]))
+            for usage in item["games3DS"]:
+                usage_data.append((j, item["name"], usage["gameName"], "3DS", usage["amiiboUsage"][0]["Usage"], usage["amiiboUsage"][0]["write"]))
+                j += 1
+
+            for usage in item["gamesWiiU"]:
+                usage_data.append((j, item["name"], usage["gameName"], "3DS", usage["amiiboUsage"][0]["Usage"], usage["amiiboUsage"][0]["write"]))
+                j += 1
+
+            for usage in item["gamesSwitch"]:
+                usage_data.append((j, item["name"], usage["gameName"], "3DS", usage["amiiboUsage"][0]["Usage"], usage["amiiboUsage"][0]["write"]))
+                j += 1
+
+            i += 1
+
+
+        c.execute("DELETE FROM amiibos")
+        c.execute("DELETE FROM usages")
+
+        sql = "INSERT INTO amiibos (id, name, head, tail, characters, type) VALUES (?, ?, ?, ?, ?, ?);"
+        c.executemany(sql, amiibo_data)
+
+
+        sql = "INSERT INTO usages (id, amiibo, game, platform, usage, write) VALUES (?, ?, ?, ?, ?, ?);"
+        c.executemany(sql, usage_data)
+
+        db.commit()
+
+    print("Successfully constructed tables")
+
+
+if __name__ == "__main__":
+    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='usages' ''')
+
+    if c.fetchone()[0]==0:
+        create_tables()
+
+    construct_amiibos()
+
+    db.close()
